ffe2285001921d1eb5b0f7b7f8647dc7f7a823f0
jonah-fro <jonah.frossard@my-digital-school.org>
2020-10-13 19:55:08 +0200

    doc(exo2.js) chargement de la bar - exo2 fini

--- exo2.js
+++ exo2.js
@@ -21,3 +21,23 @@ var drawBar = function DrawBar(sum, nbr) {
 }
 
 
+drawBar.prototype.run = function(){
+var divP = document.createElement("div");
+var divE = document.createElement("div");
+
+divP.style.width = "35%";
+divP.style.height = "50px";
+divP.style.backgroundColor = "grey";
+divP.style.borderRadius = "20px";
+
+divE.style.width = this.nbr + "%";
+divE.style.height = "100%";
+divE.style.backgroundColor ="dodgerblue";
+divE.style.borderRadius = "20px";
+
+divP.appendChild(divE);
+document.body.appendChild(divP);
+}
+
+var loadedBar = new drawBar(100, 50);
+loadedBar.run();
