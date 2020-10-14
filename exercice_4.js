dc4f0902b95e20153a7fce8c8bc5ddd533016765
jonah-fro <jonah.frossard@my-digital-school.org>
2020-10-14 23:27:35 +0200

    doc(exercice_4.js) jsdoc

--- exercice_4.js
+++ exercice_4.js
@@ -1,14 +1,16 @@
-const paths = document.getElementsByTagName("path");
-const legend = document.getElementById("legend");
-
-for(let i=0;i<paths.length;i++){
+/** récuperation des paths du svg et de la légende */
+var paths = document.getElementsByTagName("path");
+var legend = document.getElementById("legend");
+/** lorsqu'un click est effectué, supprime le précédent s'il y en a un */
+for(var i=0;i<paths.length;i++){
     paths[i].addEventListener("click",function(){
-        const clicked = document.getElementsByClassName("click");
-        for(let o=0;o<clicked.length;o++){
+        var clicked = document.getElementsByClassName("click");
+        for(var o=0;o<clicked.length;o++){
             clicked[o].classList.remove("click");
         }
 
         this.classList.add("click");
+        /** affichage de la légende */
         legend.innerText = this.getAttribute("id");
     });
 }
